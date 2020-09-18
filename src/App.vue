10b1b08cc7d56683438c7df933df6fdad6376bb1
zhangxuan <zhangxuan@16801.com>
2020-09-18 16:29:49 +0800

    myvueproject

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,33 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <keep-alive exclude="Detail">
+      <router-view></router-view>
+    </keep-alive>
+    <main-tab-bar></main-tab-bar>
+    <icon></icon>
+    <svg-icon></svg-icon>
   </div>
 </template>
 
+<script>
+  import MainTabBar from 'content/mainTabbar/MainTabBar'
+  import Icon from 'content/Icon/Icon.vue'
+  import SvgIcon from 'content/Icon/svg.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      MainTabBar,
+      Icon,
+      SvgIcon
+    }
+  }
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  @import "assets/css/base.css";
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  #app {
+    position: relative;
+  }
 </style>
